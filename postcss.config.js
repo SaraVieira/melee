20f2eb50e71ebaaf7034d633f690b0122795c8e7
Pedro Gonçalves <pedro.goncalves@mindera.com>
2017-03-16 17:40:31 +0000

    postcss.config.js edited online with Bitbucket

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,6 @@
 module.exports = {
   plugins: [
     require('postcss-smart-import'),
-    require('postcss-caralho'),
-    require('postcss-less-vars'),
-    require('postcss-cssnext')(),
-    require('postcss-nested'),
+    require('postcss-cssnext')()
   ],
 };
